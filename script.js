00644eab84f23cf3917f27b7224accc58ce34691
AlessandroRossi643 <alessandrorossi643@gmail.com>
2019-05-06 16:17:31 +0200

    Second Checkpoint

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 // Creazione Oggetto Studente; nome, cognome, età
 var studente={
-  "nome": "Alessandro",
-  "cognome": "Rossi",
-  "eta": 22
-}
+  nome: "Alessandro",
+  cognome: "Rossi",
+  eta: 22
+};
 
 // Stampa dati Studente
 for (var field in studente) {
   console.log(studente[field]);
+}
+
+console.log("--------------------");
+
+// Creazione array Studenti
+var lista_studenti={
+  nome:["Francesco","Niccolò","Luca","Maurizio"],
+  cognome:["Poni","Rinaldini","Lanzi","Pisciottu"],
+  eta:[23,21,25,39]
+};
+
+// Stampa nome & cognome array studenti
+for (var i = 0; i < lista_studenti.nome.length; i++) {
+  console.log(lista_studenti.nome[i] + " " + lista_studenti.cognome[i]);
 }
